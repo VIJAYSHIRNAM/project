fdad8fc29af4d9d9740a01ba3206f094f560116c
krish8237 <krishnaathipatla@gmail.com>
2024-07-19 19:38:29 -0500

    Added Student URL's

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 
 from app.admin import admin_bp
+from app.student import student_bp
 
 def create_ams_app():
 
@@ -8,4 +9,5 @@ def create_ams_app():
     app.template_folder = "../templates"
     app.static_folder = "../static"
     app.register_blueprint(admin_bp)
+    app.register_blueprint(student_bp)
     return app
