bc6ca178129de0c52c2f10e70d6336aac33d18f5
krish8237 <krishnaathipatla@gmail.com>
2024-07-19 19:40:29 -0500

    Changed the blueprint url_prefix

--- app/student.py
+++ app/student.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, url_for, redirect
 
-student_bp = Blueprint('admin', __name__, url_prefix='/admin/')
+student_bp = Blueprint('student', __name__, url_prefix='/')
 @student_bp.route("/")
 def home():
     return redirect(url_for('admin.index'))
